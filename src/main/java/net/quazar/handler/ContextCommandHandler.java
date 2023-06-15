da17dde210b5bda197c2cd6754802190d9cbd571
SQuazar <flawesv@yandex.ru>
2023-06-15 18:13:32 +0300

    Added cooldown handling

--- src/main/java/net/quazar/handler/ContextCommandHandler.java
+++ src/main/java/net/quazar/handler/ContextCommandHandler.java
@@ -17,8 +17,13 @@ public class ContextCommandHandler extends ListenerAdapter {
     public void onMessageContextInteraction(MessageContextInteractionEvent event) {
         if (event.getCommandString().equals("Пожаловаться")) {
             if (event.getGuild().getIdLong() != guildId) return;
-            reportCommand.execute(event);
-            return;
+            if (reportCommand.hasCooldown(event.getUser())) {
+                event.reply("Вы отправляете жалобы слишком часто!")
+                        .setEphemeral(true).queue();
+                return;
+            }
+            if (reportCommand.execute(event))
+                reportCommand.cooldown(event.getUser());
         }
     }
 }
