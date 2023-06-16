70864a8d0c8dd9762a49756b2c2ae71ee372c5b3
SQuazar <flawesv@yandex.ru>
2023-06-16 20:55:58 +0300

    Added new report command

--- src/main/java/net/quazar/handler/ContextCommandHandler.java
+++ src/main/java/net/quazar/handler/ContextCommandHandler.java
@@ -1,15 +1,18 @@
 package net.quazar.handler;
 
 import net.dv8tion.jda.api.events.interaction.command.MessageContextInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.command.UserContextInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.quazar.command.ReportContextCommand;
+import net.quazar.command.*;
 
 public class ContextCommandHandler extends ListenerAdapter {
     private final ReportContextCommand reportCommand;
+    private final ReportAvatarContextCommand reportAvatarCommand;
     private final long guildId;
 
     public ContextCommandHandler(long reportChannel, long guildId) {
         this.reportCommand = new ReportContextCommand(reportChannel);
+        this.reportAvatarCommand = new ReportAvatarContextCommand(reportChannel);
         this.guildId = guildId;
     }
 
@@ -24,6 +27,20 @@ public class ContextCommandHandler extends ListenerAdapter {
             }
             if (reportCommand.execute(event))
                 reportCommand.cooldown(event.getUser());
+        }
+    }
+
+    @Override
+    public void onUserContextInteraction(UserContextInteractionEvent event) {
+        if (event.getCommandString().equals("Пожаловаться на аватар")) {
+            if (event.getGuild().getIdLong() != guildId) return;
+            if (reportAvatarCommand.hasCooldown(event.getUser())) {
+                event.reply("Вы отправляете жалобы слишком часто!")
+                        .setEphemeral(true).queue();
+                return;
+            }
+            if (reportAvatarCommand.execute(event))
+                reportAvatarCommand.cooldown(event.getUser());
         }
     }
 }
